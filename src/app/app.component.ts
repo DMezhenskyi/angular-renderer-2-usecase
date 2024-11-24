c1e2d86b7c37c538bc6dda9fe116cf5899d8deb3
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-11-24 16:19:48 +0100

    remove some stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { InputInteraction } from './input-interaction';
-import { inputAnimation } from './animation';
 
 @Component({
   selector: 'app-root',
-  imports: [InputInteraction],
-  animations: [inputAnimation],
-  template: `
-    <input interactive-input type="search" />
-  `,
+  template: `<input type="search" />`,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
