20f6275df6b8ea46f50154d630cec9536d2ac591
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-11-26 11:57:35 +0100

    finish the state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<input type="search" />`,
-  styleUrls: ['./app.component.scss'],
+  template: `
+    <input type="search" />
+  `,
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {}
